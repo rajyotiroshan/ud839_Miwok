b273a30ddc4ea4ec8240082dca271626c9106f83
rajan jha <rajahit16@gmail.com>
2018-05-23 09:16:23 +0530

    set onClick listener on all activty. created words as an ArrayList<String>.in numbersActivity added ten words to words. activity_numbers.xml VG (RL to LL(vertical)) created TV to store word and added to numbers_activity.xml

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -21,19 +21,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        //set event listener on number view object.
+        TextView numberTv = findViewById(R.id.numbers);
+        numberTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(),NumbersActivity.class);
+               startActivity(intent);
+            }
+        });
+        //set event listener on Family members.
+        TextView FMTv = findViewById(R.id.family);
+        FMTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "a click on Family members", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //set event on colors.
+        TextView ColorsTV = findViewById(R.id.colors);
+        ColorsTV.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "a click on Colors", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //set on click event on Phrases.
+        TextView PhrasesTv = findViewById(R.id.phrases);
+        PhrasesTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "a click on Phrases", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    public void openNumbersList(View view) {
-        Intent intent = new Intent(this,NumbersActivity.class);
-        startActivity(intent);
-    }
+
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,31 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        //create an array of words.
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView=(LinearLayout)findViewById(R.id.root_view);
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+        rootView.addView(wordView);
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(1));
+        rootView.addView(wordView2);
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(words.get(2));
+        rootView.addView(wordView3);
+
+
+
     }
 }
