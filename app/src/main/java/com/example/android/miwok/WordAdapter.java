4b72ae00ed4fcc774de408a99bbd9019e3350726
rajan jha <rajahit16@gmail.com>
2018-05-27 11:55:58 +0530

    access to ImageView in WordAdapter getView()

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,6 +35,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView miwokTv = listItemView.findViewById(R.id.miwok_word);
         //set text as miwok translation.
         miwokTv.setText(currentWord.getMiwokTranslation());
+        //access ImageView and set imageResId
+        ImageView imageIV = listItemView.findViewById(R.id.img);
+        imageIV.setImageResource(currentWord.getImgResourceID());
         return listItemView;
     }
 }
