0b4b07af370e523fee293eedc9d55646839be433
rajan jha <rajahit16@gmail.com>
2018-05-27 11:33:26 +0530

    added three para constructor in Word class added mImageResId as third member var in Word class added getter method for mImageResId. change NumberActivty:called new 3 para constructor of Word obj.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -29,16 +29,16 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.words_list);
         //create an array of words.
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("lutti","one"));
-        words.add(new Word("otiiko","two"));
-        words.add(new Word("tolookosu","three"));
-        words.add(new Word("oyyiso","four"));
-        words.add(new Word("massokko","five"));
-        words.add(new Word("temmokko","six"));
-        words.add(new Word("kenekaku","seven"));
-        words.add(new Word("kawinta","eight"));
-        words.add(new Word("wo'e","nine"));
-        words.add(new Word("na'aacha","ten"));
+        words.add(new Word("lutti","one",R.drawable.number_one));
+        words.add(new Word("otiiko","two",R.drawable.number_two));
+        words.add(new Word("tolookosu","three",R.drawable.number_three));
+        words.add(new Word("oyyiso","four",R.drawable.number_four));
+        words.add(new Word("massokko","five",R.drawable.number_five));
+        words.add(new Word("temmokko","six",R.drawable.number_six));
+        words.add(new Word("kenekaku","seven",R.drawable.number_seven));
+        words.add(new Word("kawinta","eight",R.drawable.number_eight));
+        words.add(new Word("wo'e","nine",R.drawable.number_nine));
+        words.add(new Word("na'aacha","ten",R.drawable.number_ten));
         words.add(new Word("egyarah","eleven"));
         words.add(new Word("barah","twelve"));
         words.add(new Word("terah","thirteen"));
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,9 +3,17 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private  int mImgResourceID;
+    //constructor.
     public Word(String miwok,String english){
         mMiwokTranslation =miwok;
         mDefaultTranslation=english;
+    }
+    //constructor
+    Word(String mMiwokTranslation,String mDefaultTranslation, int imgResourceID){
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImgResourceID = imgResourceID;
     }
     /**
      * @return miwok word
@@ -18,5 +26,11 @@ public class Word {
      */
     public String getDefaultTranslation(){
         return this.mDefaultTranslation;
+    }
+    /**
+     * @return img res id
+     */
+    public int getImgResourceID(){
+        return mImgResourceID;
     }
 }
