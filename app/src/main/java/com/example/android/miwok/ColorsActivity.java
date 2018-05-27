34535b2ab1daab283bf2250fbff9feb110fca480
rajan jha <rajahit16@gmail.com>
2018-05-27 12:09:58 +0530

    called 3 para Word constructor in ColorsActivity  R.drawable.imageResId as 3rd argument

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -30,18 +30,18 @@ public class ColorsActivity extends AppCompatActivity {
         //creat ArrayList of Word obj
         ArrayList<Word> colors = new ArrayList<>();
         //add colors.
-        colors.add(new Word("wetetti","red"));
-        colors.add(new Word("chokokki","green"));
-        colors.add(new Word("brown","takaakki"));
+        colors.add(new Word("wetetti","red",R.drawable.color_red));
+        colors.add(new Word("chokokki","green",R.drawable.color_green));
+        colors.add(new Word("brown","takaakki",R.drawable.color_brown));
 
-        colors.add(new Word("gray","topoppi"));
-        colors.add(new Word("black","kululli"));
-        colors.add(new Word("white","kelelli"));
+        colors.add(new Word("gray","topoppi",R.drawable.color_gray));
+        colors.add(new Word("black","kululli",R.drawable.color_black));
+        colors.add(new Word("white","kelelli",R.drawable.color_white));
 
-        colors.add(new Word("dusty yellow","topiise"));
-        colors.add(new Word("mustard yellow","chiwiite"));
-        colors.add(new Word("nila","blue"));
-        colors.add(new Word("baingni","violet"));
+        colors.add(new Word("dusty yellow","topiise",R.drawable.color_dusty_yellow));
+        colors.add(new Word("mustard yellow","chiwiite",R.drawable.color_mustard_yellow));
+        colors.add(new Word("nila","blue",R.mipmap.ic_launcher));
+        colors.add(new Word("baingni","violet",R.mipmap.ic_launcher));
         //attach this colors to WordAdapter.
         WordAdapter adapter = new WordAdapter(this,colors);
         //access an adapter view here it is ListView
